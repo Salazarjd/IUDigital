e25bcd9c905a4ce65948d2f081f1e0e5f6b6b00a
Juan Diego Salazar <diego.salazar@ias.com.co>
2023-03-21 21:43:54 -0500

    configurando db

---
+++ app/src/main/java/co/com/iudigital/AdminSQLite.java
@@ -0,0 +1,25 @@
+package co.com.iudigital;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+import androidx.annotation.Nullable;
+
+public class AdminSQLite extends SQLiteOpenHelper {
+
+    public AdminSQLite(@Nullable Context context, @Nullable String name, @Nullable SQLiteDatabase.CursorFactory factory, int version) {
+        super(context, name, factory, version);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        db.execSQL("CREATE TABLE user(document integer primary key, name text, lastName text, email text, password text)");
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        db.execSQL("DROP TABLE IF EXISTS user");
+        db.execSQL("CREATE TABLE user(document integer primary key, name text, email text)");
+    }
+}
--- app/src/main/java/co/com/iudigital/PrincipalActivity.java
+++ app/src/main/java/co/com/iudigital/PrincipalActivity.java
@@ -2,15 +2,65 @@ package co.com.iudigital;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.ContentValues;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 
 public class PrincipalActivity extends AppCompatActivity {
+
+    private EditText document;
+    private EditText name;
+    private EditText lastName;
+    private EditText email;
+    private EditText password;
+
+    private Button register;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_principal);
+
+        document = findViewById(R.id.txtDocument);
+        name = findViewById(R.id.txtName);
+        lastName = findViewById(R.id.txtLastName);
+        email = findViewById(R.id.txtEmail);
+        password = findViewById(R.id.txtPassword);
+        register = findViewById(R.id.btnRegister);
+
+        register.setOnClickListener(new View.OnClickListener() {
+
+            AdminSQLite admin = new AdminSQLite(getApplicationContext(), "Biblioteca", null, 1);
+            SQLiteDatabase db = admin.getWritableDatabase();
+
+            @Override
+            public void onClick(View v) {
+                Integer doc = Integer.parseInt(document.getText().toString());
+                String na = name.getText().toString();
+                String la = lastName.getText().toString();
+                String em = email.getText().toString();
+                String pw = password.getText().toString();
+
+                ContentValues data = new ContentValues();
+                data.put("document", doc);
+                data.put("name", na);
+                data.put("lastName", la);
+                data.put("email", em);
+                data.put("password", pw);
+
+                db.insert("user", null, data);
+                db.close();
+
+                document.setText("");
+                name.setText("");
+                lastName.setText("");
+                email.setText("");
+                password.setText("");
+            }
+        });
 
 
     }
