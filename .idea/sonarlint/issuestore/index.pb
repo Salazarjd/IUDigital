f4963c16fe17a17d49cbbfec63e9f453695ce547
Juan Diego Salazar <salazar.jd@gmail.com>
2023-03-25 22:45:53 -0500

    iniciando conexion firabase

---
+++ .idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,13 @@
+
+@
+app/build.gradle,f/4/f4a01d6a4fcb971362ec00a83903fd3902f52164
+c
+3app/src/main/java/co/com/iudigital/AdminSQLite.java,8/e/8ef8071a1d6a09894e94ef25dacc51fca906a58b
+d
+4app/src/main/java/co/com/iudigital/MainActivity.java,a/4/a4fc0077d9acf6a2ee7b7dc35991f1fe50013fc7
+h
+8app/src/main/java/co/com/iudigital/RegisterActivity.java,9/1/91505e7733a75702f4d126756b0352434574068a
+\
+,app/src/main/java/co/com/iudigital/User.java,7/2/72c2478f4836ad3f84f96788c0072f51aaeb2206
+i
+9app/src/main/java/co/com/iudigital/PrincipalActivity.java,b/3/b33de669021b316ca6c88d153e2b20dd4492a0b5
--- app/src/main/java/co/com/iudigital/PrincipalActivity.java
+++ app/src/main/java/co/com/iudigital/PrincipalActivity.java
@@ -1,5 +1,6 @@
 package co.com.iudigital;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.ContentValues;
@@ -8,6 +9,13 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class PrincipalActivity extends AppCompatActivity {
 
@@ -17,7 +25,7 @@ public class PrincipalActivity extends AppCompatActivity {
     private EditText email;
     private EditText password;
 
-    private Button register;
+    private Button btnRegister;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,39 +37,114 @@ public class PrincipalActivity extends AppCompatActivity {
         lastName = findViewById(R.id.txtLastName);
         email = findViewById(R.id.txtEmail);
         password = findViewById(R.id.txtPassword);
-        register = findViewById(R.id.btnRegister);
+        btnRegister = findViewById(R.id.btnRegister);
 
-        register.setOnClickListener(new View.OnClickListener() {
+        registerUser();
 
-            AdminSQLite admin = new AdminSQLite(getApplicationContext(), "Biblioteca", null, 1);
-            SQLiteDatabase db = admin.getWritableDatabase();
+//        register.setOnClickListener(new View.OnClickListener() {
+//
+//            AdminSQLite admin = new AdminSQLite(getApplicationContext(), "Biblioteca", null, 1);
+//            SQLiteDatabase db = admin.getWritableDatabase();
+//
+//            @Override
+//            public void onClick(View v) {
+//                Integer doc = Integer.parseInt(document.getText().toString());
+//                String na = name.getText().toString();
+//                String la = lastName.getText().toString();
+//                String em = email.getText().toString();
+//                String pw = password.getText().toString();
+//
+//                ContentValues data = new ContentValues();
+//                data.put("document", doc);
+//                data.put("name", na);
+//                data.put("lastName", la);
+//                data.put("email", em);
+//                data.put("password", pw);
+//
+//                db.insert("user", null, data);
+//                db.close();
+//
+//                document.setText("");
+//                name.setText("");
+//                lastName.setText("");
+//                email.setText("");
+//                password.setText("");
+//            }
+//        });
+
+
+    }
+
+    public void registerUser(){
+        btnRegister.setOnClickListener(new View.OnClickListener() {
 
             @Override
-            public void onClick(View v) {
-                Integer doc = Integer.parseInt(document.getText().toString());
-                String na = name.getText().toString();
+            public void onClick(View view) {
+
+                boolean checkValues = verifyInputs();
+                if(checkValues){
+                    return;
+                }
+
+                String id = document.getText().toString();
+                String nam = name.getText().toString();
                 String la = lastName.getText().toString();
                 String em = email.getText().toString();
                 String pw = password.getText().toString();
 
-                ContentValues data = new ContentValues();
-                data.put("document", doc);
-                data.put("name", na);
-                data.put("lastName", la);
-                data.put("email", em);
-                data.put("password", pw);
+                FirebaseDatabase db = FirebaseDatabase.getInstance();
+                DatabaseReference dbref = db.getReference(User.class.getSimpleName());
 
-                db.insert("user", null, data);
-                db.close();
+                dbref.addListenerForSingleValueEvent(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(@NonNull DataSnapshot snapshot) {
+                        User user = new User(id, nam, la, em, pw);
+                        dbref.push().setValue(user);
+                        Toast.makeText(PrincipalActivity.this, "Usuario Registrado", Toast.LENGTH_SHORT).show();
+                        resetValues();
+                    }
 
-                document.setText("");
-                name.setText("");
-                lastName.setText("");
-                email.setText("");
-                password.setText("");
+                    @Override
+                    public void onCancelled(@NonNull DatabaseError error) {
+
+                    }
+                });
             }
         });
+    }
 
+    public boolean verifyInputs() {
+        boolean result = false;
 
+        if(document.getText().toString().isEmpty()){
+            document.setError("Campo obligatorio");
+            result = true;
+        }
+        if(name.getText().toString().isEmpty()){
+            name.setError("Campo obligatorio");
+            result = true;
+        }
+        if(lastName.getText().toString().isEmpty()){
+            lastName.setError("Campo obligatorio");
+            result = true;
+        }
+        if(email.getText().toString().isEmpty()){
+            email.setError("Campo obligatorio");
+            result = true;
+        }
+        if(password.getText().toString().isEmpty()){
+            password.setError("Campo obligatorio");
+            result = true;
+        }
+
+        return result;
+    }
+
+    public void resetValues() {
+        document.setText("");
+        name.setText("");
+        lastName.setText("");
+        email.setText("");
+        password.setText("");
     }
 }
